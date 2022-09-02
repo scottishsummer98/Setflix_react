860bd6a831f2ca38dc212412f48d53b68710c18b
scottishsummer98 <samiremail98@gmail.com>
2022-09-02 09:01:36 +0600

    'initial'

---
+++ src/request.js
@@ -0,0 +1,14 @@
+const API_KEY = '74cad9533a13b014d0a68711b64a1748'
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en=US`,
+  fetchSetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanticMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests
